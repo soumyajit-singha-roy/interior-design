e6bd816f3ca8f599625ab24fc792c854f63ffc3b
soumyajit-singha-roy <soumyajitsingharoy005@gmail.com>
2026-05-06 17:25:33 +0530

    Configure deployment to GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/interior-design',
   experimental: {
     turbopack: {
       root: process.cwd(),
